b9aa80f6813d52631a877924200b3478a1f6b26d
biboking-373 <vinnikinyua7@gmail.com>
2023-12-01 02:16:02 -0800

    1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,29 +1,14 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from calculator_1 import add
+    from calculator_1 import add,sub,mul,div
     a = 10
     b = 5
-    result = add(a, b)
-    print("{:d} + {:d} = {:d}".format(a, b, result))
-
-if __name__ == "__main__":
-    from calculator_1 import sub
-    a = 10
-    b = 5
-    result = sub(a, b)  # Corrected the function name to sub
-    print("{:d} - {:d} = {:d}".format(a, b, result))
-
-if __name__ == "__main__":
-    from calculator_1 import mul
-    a = 10
-    b = 5
-    result = mul(a, b)  # Corrected the function name to mul
-    print("{:d} * {:d} = {:d}".format(a, b, result))
-
-if __name__ == "__main__":
-    from calculator_1 import div
-    a = 10
-    b = 5
-    result = div(a, b)  # Corrected the function name to div
-    print("{:d} / {:d} = {:d}".format(a, b, result))
+    add_result = add(a, b)
+    sub_result = sub(a, b)
+    mul_result = mul(a, b)
+    div_result = div(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, add_result))
+    print("{:d} - {:d} = {:d}".format(a, b, sub_result))
+    print("{:d} * {:d} = {:d}".format(a, b, mul_result))
+    print("{:d} / {:d} = {:d}".format(a, b, div_result))
